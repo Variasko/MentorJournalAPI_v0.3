3e7641a3066264573fcdd4ec9840a92743ab4106
Variasko <121598389+Variasko@users.noreply.github.com>
2024-10-31 09:08:46 +0300

    fork test commit

--- MentorJournalAPI_v0.3/Services/SocialStatusService.cs
+++ MentorJournalAPI_v0.3/Services/SocialStatusService.cs
@@ -9,6 +9,10 @@ namespace MentorJournalAPI_v0._3.Services
 {
 public class SocialStatusService : ISocialStatusService
     {
-        
+        public async Task<IEnumerable<SocialStatusDto>> GetAllAsync();
+        public async Task<SocialStatusDto> GetByIdAsync(int id);
+        public async Task<SocialStatusDto> CreateAsync(SocialStatusDto socialstatusDto);
+        public async Task<SocialStatusDto> UpdateAsync(int id, SocialStatusDto socialstatusDto);
+        public async Task DeleteAsync(int id);
     }
 }
